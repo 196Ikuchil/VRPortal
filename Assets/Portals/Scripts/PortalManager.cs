f604d264d05246e4cd5dce41c28b365ad0c1a25d
komiyamalab <komiyamalab.share@gmail.com>
2017-05-03 16:09:19 +0900

    クオータニオンのしゅうせい

--- Assets/Portals/Scripts/PortalManager.cs
+++ Assets/Portals/Scripts/PortalManager.cs
@@ -5,21 +5,24 @@ public class PortalManager : MonoBehaviour {
 
     public GameObject[] RenderTextureCameras;
     public GameObject[] Portals;
-    Vector3 init;
+   
     int _portalIndex;
+    Vector3 init;
 
     void Start()
     {
-
+        init = transform.position;
     }
+
     // Update is called once per frame
     void Update () {
         _portalIndex = 0;
 
         foreach(var camera in RenderTextureCameras) {
             //camera.transform.localPosition = transform.position - Portals[_portalIndex].transform.position;
-            camera.transform.localPosition = - Portals[_portalIndex].transform.position;
-            camera.transform.localRotation = transform.localRotation;
+            //camera.transform.localPosition = - Portals[_portalIndex].transform.position;
+            camera.transform.localPosition = (transform.position - Portals[_portalIndex].transform.position) * 0.2F;
+            camera.transform.localRotation = Quaternion.Euler((Portals[_portalIndex].transform.position - transform.position).x, 0, 0);// * transform.localRotation;
             _portalIndex++;
         }
 	}
