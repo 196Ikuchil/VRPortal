5f24b38812da34ee11aeaf2982f657d892839b9b
komiyamalab <komiyamalab.share@gmail.com>
2017-05-03 14:31:25 +0900

    エラーなし、正常起動、oculus対応

---
+++ Assets/CameraMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class CameraMaster : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //// TODO: ここで固定したい位置があれば指定しておく
+        //Vector3 basePos = Vector3.zero;
+
+        //// VR.InputTracking から hmd の位置を取得
+        //Vector3 trackingPos =
+        //        InputTracking.GetLocalPosition(VRNode.CenterEye);
+
+        //// CameraController 自体の rotation が
+        //// zero でなければ rotation を掛ける
+        //// trackingPosition = trackingPos * transform.rotation;
+
+        //// 固定したい位置から hmd の位置を
+        //// 差し引いて実質 hmd の移動を無効化する
+        //transform.position = basePos - trackingPos;
+
+    }
+}
--- Assets/Portals/Scripts/PortalManager.cs
+++ Assets/Portals/Scripts/PortalManager.cs
@@ -7,13 +7,14 @@ public class PortalManager : MonoBehaviour {
     public GameObject[] Portals;
 
     int _portalIndex;
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         _portalIndex = 0;
 
         foreach(var camera in RenderTextureCameras) {
-            camera.transform.localPosition = transform.position - Portals[_portalIndex].transform.position;
+            //camera.transform.localPosition = transform.position - Portals[_portalIndex].transform.position;
+            camera.transform.localPosition = - Portals[_portalIndex].transform.position;
             camera.transform.localRotation = transform.localRotation;
             _portalIndex++;
         }
