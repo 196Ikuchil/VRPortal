5588670f4b7ace54a1c60d0ab6804db7f8f589dc
196kakinuma <196thinline@gmail.com>
2017-05-06 14:37:17 +0900

    terrain追加、整理

---
+++ Assets/Scripts/Portal/PortaParent.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortaParent : MonoBehaviour {
+
+    [SerializeField]
+    Portal mainPortal;
+    [SerializeField]
+    Portal subPortal;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public Portal GetMainPortal()
+    {
+        return mainPortal;
+    }
+   public  Portal GetSubPortal()
+    {
+        return subPortal;
+    }
+
+    void SetMainPortalCamera(PortalCamera camera)
+    {
+        GetMainPortal().SetPortalCamera(camera);
+    }
+
+    void SetSubPortalCamera(PortalCamera camera)
+    {
+        GetSubPortal().SetPortalCamera(camera);
+    }
+
+    /// <summary>
+    /// 個々の持つポータルの可視を設定する
+    /// trueならsubの世界にいるということ
+    /// </summary>
+    /// <param name="anotherWorld"></param>
+    public void SetPortalvisible(bool anotherWorld)
+    {
+        if(anotherWorld) //subにいる
+        {
+            mainPortal.gameObject.SetActive(false);
+            mainPortal.transform.parent.gameObject.SetActive(false);
+            mainPortal.StartAmin = false;
+            subPortal.gameObject.SetActive(true);
+            subPortal.transform.parent.gameObject.SetActive(true);
+        }
+        else
+        {
+            mainPortal.gameObject.SetActive(true);
+            mainPortal.transform.parent.gameObject.SetActive(true);
+            subPortal.gameObject.SetActive(false);
+            subPortal.transform.parent.gameObject.SetActive(false);
+            subPortal.StartAmin = false;
+        }
+
+    }
+
+    public void SetPortalCamera(PortalCamera main,PortalCamera sub)
+    {
+        SetMainPortalCamera(sub); //名前のつけ方
+        SetSubPortalCamera(main);
+    }
+}
---
+++ Assets/Scripts/Portal/PortalManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PortalManager : MonoBehaviour {
+
+    [SerializeField]
+    List<PortaParent> portalParents;
+
+    [SerializeField]
+    PortalCamera mainPortalCamera;
+    [SerializeField]
+    PortalCamera subPortalCamera;
+
+    [SerializeField]
+    GameObject mainCamera;
+
+
+    [SerializeField]
+    GameObject portalPref;
+   
+    int _portalIndex;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mainPortalCamera.GetMyPortalCamera().transform.localPosition = Vector3.zero;
+        mainPortalCamera.GetMyPortalCamera().transform.localRotation = mainCamera.transform.localRotation;
+
+        subPortalCamera.GetMyPortalCamera().transform.localPosition = Vector3.zero;
+        subPortalCamera.GetMyPortalCamera().transform.localRotation = mainCamera.transform.localRotation;
+    }
+
+
+    public IEnumerator PortalVisibleManage(bool anotherWorld,float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        foreach (PortaParent parent in portalParents)
+        {
+            parent.SetPortalvisible(anotherWorld);
+        }
+
+    }
+
+    /// <summary>
+    /// 指定したポジションにポータルを作成する
+    /// </summary>
+    public void CreatePortal(Vector3 position,Vector3 forward)
+    {
+        forward.y = 0;
+        position.y = 0;
+
+        var parent =Instantiate(portalPref, position, Quaternion.identity).GetComponent<PortaParent>();
+        parent.transform.forward = forward;
+
+        portalParents.Add(parent);
+        parent.SetPortalCamera(mainPortalCamera,subPortalCamera);
+
+        if (GameMaster.Instance.IsAnotherWorld())//subなら
+        {
+            parent.transform.forward = -forward;
+            parent.SetPortalvisible(true);
+        }
+    }
+}
